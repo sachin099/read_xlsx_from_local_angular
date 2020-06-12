4b6ad0ac7f181e78a6ee72b8440cbd0c7a5b32b0
unknown <sachin.choudhary58@gmail.com>
2020-06-12 22:35:38 +0530

    Adding folders and code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+  data : any;
   title = 'xlsx';
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.http.get("assets/test.xlsx", { responseType: 'blob' }).subscribe(data => {      
+      const reader: FileReader = new FileReader();
+      reader.readAsBinaryString(data);
+      reader.onload = (e: any) => {
+      const binarystr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(binarystr, { type: 'binary' });
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+      const data = XLSX.utils.sheet_to_json(ws);
+      this.data = data;
+      console.log(data);
+    };
+  })
+  
+} 
 }
+
